24c72b316793b8c6a85444bfa95e5bf4e24bd179
JashKarani <karanijash@gmail.com>
2022-12-12 21:04:40 +0530

    completed user connect

--- dating_site/dating/views.py
+++ dating_site/dating/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView,DetailView,CreateView,UpdateView,Delet
 from django.contrib.auth.models import User
 from users.models import Profile
 from dating.models import Chatrequests
+from django.shortcuts import redirect
+
+
 
 # Create your views here.
 
@@ -12,9 +15,17 @@ class  UsersList(ListView):
     context_object_name='users'
     def post(self, request):
         user_requested = request.POST['chat_request']
-        json_file=request.user.chatrequests.chat_request
+        
+        try:
+            json_file=request.user.chatrequests.chat_request
+        except:
+            chat_request_var=Chatrequests.objects.create(user=request.user)
+            json_file=request.user.chatrequests.chat_request
         json_file.append(user_requested)
-        json_file.save()
+        a=Chatrequests.objects.get(user=request.user)
+        a.chat_request=json_file
+        a.save()
+        return redirect('/')
 
             
             
